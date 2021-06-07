4451e00f5f2b8186047d060daab57a3a9d079a6e
RajeshAbhimanyu <rajeshthokala10@gmail.com>
2021-06-07 10:58:42 +0530

    initial commit of springboot react full stack

---
+++ SpringBootFullStackApp/src/main/java/com/springboot/fullstack/SpringBootFullStackAppApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFullStackAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFullStackAppApplication.class, args);
+	}
+
+}
